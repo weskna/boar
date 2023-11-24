9cbb7bebd7e64684c3417a58d35205c1c61c79fd
Vesna Klopčič <koweskna@gmail.com>
2023-11-24 19:47:10 +0100

    Initial commit

---
+++ src/main/java/com/github/weskna/boar/BoarApplication.java
@@ -0,0 +1,13 @@
+package com.github.weskna.boar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/weskna/boar/BoarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.weskna.boar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
